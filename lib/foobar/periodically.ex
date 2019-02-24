581e888914ccd55645514e3e47c318bd19051ce0
Bobby 'Bobdawg' Grayson <bobbbygrayson@gmail.com>
2019-02-24 17:51:47 -0500

    add a thing

---
+++ lib/foobar/periodically.ex
@@ -0,0 +1,33 @@
+defmodule Foobar.Periodically do
+  use GenServer
+
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, %{})
+  end
+
+  @impl true
+  def init(state) do
+    # Schedule work to be performed on start
+    schedule_work()
+
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_info(:work, state) do
+    # we should listen and see if the helper
+    # file has any modules written to it and if so recompile that shiz
+    # and then log
+    
+    # Reschedule once more
+    schedule_work()
+
+    {:noreply, state}
+  end
+
+  defp schedule_work do
+    Process.send_after(self(), :work, :timer.seconds(1))
+  end
+end
+
+
